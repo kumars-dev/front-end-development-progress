0348a7b0decae6a9cc0acaf2ed05f8ae477628c1
montu <coder.Dev.kumar98@gmail.com>
2025-02-04 01:06:31 -0800

    starting phase of initialization of project portfolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+ <h1 className='text-4xl'>Welcome respective hr or my dear clients</h1>
+ <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur commodi repudiandae quae officia sed. Minus aliquid minima ad odit, molestias ducimus quasi autem eos error, eum asperiores veritatis possimus. Quo earum asperiores nisi ut error.</p>
+ <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatibus optio, voluptas quo omnis, sint recusandae temporibus officia libero repellendus iste totam, a id at.</p>
+    </>
+  )
+}
+
+export default App
