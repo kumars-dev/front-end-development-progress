7f767470e2f5adb8dc9a91d7155da6a62101f143
montukumar <montukumarpandit19888@gmail.com>
2025-09-19 14:17:53 +0530

    today i have done changes in screen responsiveness upto innerservices section....

--- script.js
+++ script.js
@@ -29,16 +29,16 @@ let closeBtn = document.querySelector("#mobilescreen i");
 let menuItems = document.querySelectorAll("#mobilescreen ul a");
 
 // Add click handlers to menu items to close menu when clicked
-menuItems.forEach(item => {
-    item.addEventListener("click", () => {
-        closeMobileMenu();
-    });
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    closeMobileMenu();
+  });
 });
 
 // Open menu handler
 document.querySelector("nav > #menu").addEventListener("click", function () {
-    closemobilescreen.style.display = "flex";
-    tl.play();
+  closemobilescreen.style.display = "flex";
+  tl.play();
 });
 
 // Close menu handler
@@ -46,19 +46,65 @@ closeBtn.addEventListener("click", closeMobileMenu);
 
 // Function to handle closing the mobile menu
 function closeMobileMenu() {
-    tl.reverse();
-    // Hide the menu after animation completes
-    setTimeout(() => {
-        closemobilescreen.style.display = "none";
-        tl.pause();
-        tl.progress(0);
-    }, tl.duration() * 100);
+  tl.reverse();
+  // Hide the menu after animation completes
+  setTimeout(() => {
+    closemobilescreen.style.display = "none";
+    tl.pause();
+    tl.progress(0);
+  }, tl.duration() * 100);
 }
 
+// smooth scrolling use lenis
 
-// gallery animation scroll
+const vertical_selector = document.querySelector(".gallery-container");
+const leftcolumn = document.querySelector(".leftbox");
+const rightcolumn = document.querySelector(".rightbox");
+const images = gsap.utils.toArray(".gallery-item img");
+gsap
+  .timeline({
+    defaults: {
+      ease: "none",
+    },
+    scrollTrigger: {
+      trigger: "#gallery",
+      start: "top top",
+      end: `+=${images.length * 50}`,
+      scrub: 2,
+      invaliddateOnRefresh: true,
+  //      markers:{
+  //   fontSize:"20px"
+  // },
+    },
+  })
+  .to(leftcolumn, {
+    y: () => -(leftcolumn.offsetHeight - vertical_selector.offsetHeight),
+  })
+  .fromTo(
+    rightcolumn,
+    {
+      y: () => vertical_selector.offsetHeight - rightcolumn.offsetHeight,
+    },
+    { y: 0 },
+    "<"
+  );
+ScrollTrigger.create({
+  trigger: "#gallery",
+  start: "top top",
+  end: `+=${images.length * 40}`,
+  invaliddateOnRefresh: true,
+  pin:true,
+  // markers:{
+  //   fontSize:"20px"
+  // }
+});
+let blank_space = document.querySelector('.blank-section')
+const styleobject ={
+  border:"1px solid red",
+  height:"60vh",
+  background:"#1c5b7bb9"
+}
 
-
-
-
-
+// blank_space.style.border = styleobject.border;
+blank_space.style.height = styleobject.height;
+blank_space.style.background = styleobject.background;
